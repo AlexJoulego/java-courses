159ec2673c748a208bf1e7560a89a94dd0fd6161
Alexander Joulego <alexjoul@gmail.com>
2017-04-16 03:35:22 +0300

    lesson 3 task done

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,18 @@
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int sum = first + second;
+		int diff = first - second;
+		int product = first * second;
+		int ratio = first / second;
+		int pow = (int)Math.pow(first, second);
+
+		System.out.println("Sum = " + sum);
+		System.out.println("Difference = " + diff);
+		System.out.println("Product = " + product);
+		System.out.println("Ratio = " + ratio);
+		System.out.println("Power = " + pow);
 	}
 }
